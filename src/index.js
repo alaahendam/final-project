545634e2ef17c1db532855f4ecd64d0322ae2e96
alaahendam <47250751+alaahendam@users.noreply.github.com>
2021-05-20 02:26:02 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
     </BrowserRouter>
   </React.StrictMode>,
